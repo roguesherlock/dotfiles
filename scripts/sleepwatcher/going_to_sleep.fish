3f74cee2c275075f10c0c31ff1f330abc8cac1c6
akash <pomalakash@gmail.com>
2019-01-27 14:26:52 +0530

    don't update status during weekends

--- scripts/sleepwatcher/going_to_sleep.fish
+++ scripts/sleepwatcher/going_to_sleep.fish
@@ -1,4 +1,8 @@
 #!/usr/local/bin/fish
 
+set day (date +"%a")
+if test $day = "Sun" -o $day = "Sat"
+  exit 0
+end
 slack_presence "afk"
 slack_presence "afk :afk:" --set-status
--- scripts/sleepwatcher/waking_from_sleep.fish
+++ scripts/sleepwatcher/waking_from_sleep.fish
@@ -1,5 +1,9 @@
 #!/usr/local/bin/fish
 
+set day (date +"%a")
+if test $day = "Sun" -o $day = "Sat"
+  exit 0
+end
 # wait for wifi
 sleep 120
 slack_presence "back"
