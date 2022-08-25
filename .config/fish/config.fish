d48f585701ee6e921101127389cb639af43c6aa7
Akash <aakash@hey.com>
2022-08-25 14:55:12 +0530

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,32 @@
 # test -z "$TMUX"; and exec tmux
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    # aliases
+    alias vim=nvim
+    alias vi=nvim
+    alias pg='ping www.google.com'
+    alias gl='git log --oneline --graph --all --decorate'
+    alias gc='git checkout'
+    alias gp='git pull'
+    alias yt="yt-dlp "
+    alias ytb="yt-dlp -f 'bv+ba/b' "
+    alias yta="yt-dlp -f 'ba' -S 'ext' "
+
+    # add bunch of fish functions
+    source ~/.config/fish/functions/fish_functions.fish
+
+    eval (direnv hook fish)
+
+    test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
+
+    # Bun
+    set -Ux BUN_INSTALL "/Users/akash/.bun"
+    set -px --path PATH "/Users/akash/.bun/bin" "/Users/akash/.local/bin"
 end
 export GPG_TTY=(tty)
 
-# aliases
-alias vim=nvim
-alias vi=nvim
-alias pg='ping www.google.com'
-alias gl='git log --oneline --graph --all --decorate'
-alias gc='git checkout'
-alias gp='git pull'
-alias yt="yt-dlp "
-alias ytb="yt-dlp -f 'bv+ba/b' "
-alias yta="yt-dlp -f 'ba' -S 'ext' "
-
-# add bunch of fish functions
-source ~/.config/fish/functions/fish_functions.fish
-
-eval (direnv hook fish)
-
-test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
-
 set -x LC_ALL en_US.UTF-8
-
 
 # On macOS, the default number of file descriptors is far too low ('256').
 # That would cause Docker to run out of available file handles, so let's
@@ -35,7 +38,4 @@ ulimit -n 2048
 # 60 seconds, which is too little. Bump it up (in seconds) to 5 minutes.
 set -x COMPOSE_HTTP_TIMEOUT 300
 
-# Bun
-set -Ux BUN_INSTALL "/Users/akash/.bun"
-set -px --path PATH "/Users/akash/.bun/bin" "/Users/akash/.local/bin"
 
