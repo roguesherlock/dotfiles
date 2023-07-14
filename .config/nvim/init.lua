bd058609c7ec9573d4f17a2ccf2839ede701844e
Akash <aakash@hey.com>
2023-07-14 08:45:31 +0530

    dump

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,18 +1,23 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system(
-        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-         lazypath})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Use a protected call so we don"t error out on first use
 local status_ok, packer = pcall(require, "lazy")
 if not status_ok then
-    return
+  return
 end
 
-require("lazy").setup('user.plugins')
+require("lazy").setup("user.plugins")
 require "user.options"
 require "user.keymaps"
 require "user.autocommands"
