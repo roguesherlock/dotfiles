a5b001f70349c459393f4cf5f4cbbbb9d8ae708d
Akash <aakash@hey.com>
2023-07-14 08:38:21 +0530

    init lazy.vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,20 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system(
+        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+         lazypath})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Use a protected call so we don"t error out on first use
+local status_ok, packer = pcall(require, "lazy")
+if not status_ok then
+    return
+end
+
+require("lazy").setup('user.plugins')
 require "user.options"
 require "user.keymaps"
-require "user.plugins"
 require "user.autocommands"
 require "user.colorscheme"
 require "user.cmp"
@@ -19,7 +33,7 @@ require "user.illuminate"
 require "user.indentline"
 require "user.alpha"
 require "user.leap"
-require "user.lsp"
+-- require "user.lsp"
 -- require "user.dap"
 require "user.diffview"
 require "user.workspace"
