1f7482711383caaf98dc7a77efdaaf95ec3931a5
Akash <aakash@hey.com>
2023-07-30 00:12:24 +0530

    init wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,44 @@
+-- Pull in the wezterm API
+local wezterm = require "wezterm"
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font_size = 14.0
+config.line_height = 1.2
+
+local appearance = wezterm.gui.get_appearance()
+if appearance:find "Dark" then
+  -- config.color_scheme = "Catppuccin Frappe"
+  config.color_scheme = "GruvboxDark"
+else
+  -- config.color_scheme = "Catppuccin Latte"
+  config.color_scheme = "Gruvbox Light"
+end
+
+config.window_decorations = "RESIZE"
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0
+}
+config.adjust_window_size_when_changing_font_size = false
+
+config.hide_tab_bar_if_only_one_tab = true
+config.enable_scroll_bar = false
+
+-- config.enable_kitty_keyboard = true
+-- config.enable_csi_u_key_encoding = false
+
+-- config.window_background_opacity = 0.7
+-- config.macos_window_background_blur = 20
+
+-- and finally, return the configuration to wezterm
+return config
