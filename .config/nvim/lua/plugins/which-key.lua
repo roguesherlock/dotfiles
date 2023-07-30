303e87c3b9627cd34a4a036fe3569f03510bc1f6
Akash <aakash@hey.com>
2023-07-30 19:54:43 +0530

    update which key description

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -5,7 +5,8 @@ return {
 			defaults = {
 				["<leader>z"] = { name = "Zen" },
 				["<leader>j"] = { name = "Find Files (Git)" },
-				["<leader><tab>"] = { name = "Buffer list" },
+				["<leader>sl"] = { name = "Live Grep (with args)" },
+				["<leader><tab>"] = { name = "Buffer list (with preview)" },
 				["<leader>t"] = { name = "+tabs" },
 				["<leader>m"] = { name = "+harpoon" },
 			},
