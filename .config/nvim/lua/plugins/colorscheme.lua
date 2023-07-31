fe741b002a430fe4aabb9cdc9da5ba6f772feb02
Akash <aakash@hey.com>
2023-07-31 19:20:40 +0530

    update colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,40 @@
 return {
-  -- add gruvbox
-  {
-    "ellisonleao/gruvbox.nvim"
-  },
-    { 'rose-pine/neovim', name = 'rose-pine' },
+	-- add gruvbox
+	{
+		"ellisonleao/gruvbox.nvim",
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				highlight_groups = {
+					IndentBlanklineChar = { fg = "highlight_low" },
+					IndentBlanklineContextChar = { fg = "highlight_high" },
+					IndentBlanklineContextStart = { sp = "highlight_high" },
+				},
+			})
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		overrides = function(colors)
+			local theme = colors.theme
+			return {
+				IndentBlanklineChar = { fg = theme.ui.bg_p1 },
+				IndentBlanklineContextChar = { fg = theme.ui.bg_p2 },
+				IndentBlanklineContextStart = { sp = theme.ui.bg_p2 },
+			}
+		end,
+	},
 
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "gruvbox"
-      -- colorscheme = "catppuccin"
-      colorscheme = "rose-pine"
-    }
-  }
+	-- Configure LazyVim to load gruvbox
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			-- colorscheme = "gruvbox"
+			-- colorscheme = "catppuccin"
+			colorscheme = "rose-pine",
+		},
+	},
 }
