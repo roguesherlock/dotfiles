c8fce7cce48bf68486a2ba7d714a31a723e8192d
Akash <aakash@hey.com>
2023-08-03 18:52:54 +0530

    update drak mode check fn

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
+--  TODO: add dark mode detection
 -- https://github.com/jascha030/macos-nvim-dark-mode
-local os_is_dark = function()
-	return (vim.call(
-		"system",
-		[[echo $(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo 'dark' || echo 'light')]]
-	)):find("dark") ~= nil
-end
+-- local os_is_dark = function()
+-- 	return (vim.call(
+-- 		"system",
+-- 		[[echo $(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo 'dark' || echo 'light')]]
+-- 	)):find("dark") ~= nil
+-- end
 
 return {
 	-- add gruvbox
