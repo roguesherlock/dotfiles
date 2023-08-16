c26ef49fde0bea1ddf5f70abc867360367a62178
Akash <aakash@hey.com>
2023-08-16 22:22:53 +0530

    update which key

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -10,6 +10,12 @@ return {
 				["<leader>t"] = { name = "+tabs" },
 				["<leader>m"] = { name = "+harpoon" },
 			},
+			triggers_blacklist = {
+				-- list of mode / prefixes that should never be hooked by WhichKey
+				-- this is mostly relevant for keymaps that start with a native binding
+				i = { "j", "k", "f" },
+				v = { "j", "k" },
+			},
 		},
 	},
 }
