9b4de6c678c745f04e2ee3134f11e482cf145092
Akash <aakash@hey.com>
2023-10-06 17:20:15 +0530

    update project

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -6,6 +6,10 @@ return {
 				"~/Developer/*",
 			},
 		},
+		init = function()
+			-- enable saving the state of plugins in the session
+			vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+		end,
 		dependencies = { "nvim-telescope/telescope.nvim", "Shatur/neovim-session-manager" },
 		lazy = false,
 		priority = 100,
