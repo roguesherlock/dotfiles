ad39280c3a72be23478a118c13c0c4f4c77b6319
Akash <aakash@hey.com>
2023-10-29 21:28:18 +0530

    update colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -13,8 +13,18 @@ return {
 	-- 	"bluz71/vim-nightfly-guicolors",
 	-- 	priority = 1000, -- make sure to load this before all the other start plugins
 	-- 	config = function()
+	-- 		vim.g.nightflyTransparent = true
 	-- 		-- load the colorscheme here
 	-- 		-- vim.cmd([[colorscheme nightfly]])
+	-- 	end,
+	-- },
+	-- {
+	-- 	"bluz71/vim-moonfly-colors",
+	-- 	name = "moonfly",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.g.moonflyTransparent = true
 	-- 	end,
 	-- },
 	-- {
@@ -29,17 +39,108 @@ return {
 	-- 		})
 	-- 	end,
 	-- },
-	-- {
-	-- 	"sainnhe/gruvbox-material",
-	-- },
+	{
+		"rose-pine/neovim",
+		lazy = false,
+		name = "rose-pine",
+		opts = {
+			variant = "auto",
+			dark_variant = "moon",
+			disable_background = true,
+			disable_float_background = true,
+			disable_italics = true,
+			highlight_groups = {
+				FloatBorder = { fg = "subtle", bg = "none" },
+				TelescopeBorder = { fg = "subtle", bg = "none" },
+				TelescopeNormal = { fg = "none" },
+				TelescopePromptNormal = { bg = "none" },
+				TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+				TelescopeSelection = { fg = "text", bg = "text", blend = 10 },
+				TelescopeSelectionCaret = { fg = "base", bg = "text" },
+				Cursor = { fg = "base", bg = "text" },
+				ColorColumn = { bg = "rose" },
+				CursorLine = { bg = "text", blend = 30 },
+				StatusLine = { fg = "love", bg = "love", blend = 10 },
+				StatusLineNC = { fg = "subtle", bg = "surface" },
+				GitSignsAdd = { fg = "iris", bg = "none" },
+				GitSignsChange = { fg = "foam", bg = "none" },
+				GitSignsDelete = { fg = "rose", bg = "none" },
+			},
+		},
+	},
+
+	{
+		"sainnhe/gruvbox-material",
+		name = "gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_better_performance = 1
+			-- Fonts
+			vim.g.gruvbox_material_disable_italic_comment = 1
+			vim.g.gruvbox_material_enable_italic = 0
+			vim.g.gruvbox_material_enable_bold = 0
+			vim.g.gruvbox_material_transparent_background = 1
+			-- Themes
+			vim.g.gruvbox_material_foreground = "mix"
+			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
+			vim.g.gruvbox_material_float_style = "dim" -- Background of floating windows
+
+			local configuration = vim.fn["gruvbox_material#get_configuration"]()
+			local palette = vim.fn["gruvbox_material#get_palette"](
+				configuration.background,
+				configuration.foreground,
+				configuration.colors_override
+			)
+
+			vim.cmd.colorscheme("gruvbox-material")
+
+			local highlights_groups = {
+				FoldColumn = { bg = "none" },
+				SignColumn = { bg = "none" },
+				Normal = { bg = "none" },
+				NormalNC = { bg = "none" },
+				NormalFloat = { bg = "none" },
+				FloatBorder = { bg = "none" },
+				FloatTitle = { bg = "none", fg = palette.orange[1] },
+				TelescopeBorder = { bg = "none" },
+				TelescopeNormal = { fg = "none" },
+				TelescopePromptNormal = { bg = "none" },
+				TelescopeResultsNormal = { bg = "none" },
+				TelescopeSelection = { bg = palette.bg3[1] },
+				Visual = { bg = palette.bg_visual_red[1] },
+				Cursor = { bg = palette.bg_red[1], fg = palette.bg_dim[1] },
+				ColorColumn = { bg = palette.bg_visual_blue[1] },
+				CursorLine = { bg = palette.bg3[1], blend = 25 },
+				GitSignsAdd = { fg = palette.green[1], bg = "none" },
+				GitSignsChange = { fg = palette.yellow[1], bg = "none" },
+				GitSignsDelete = { fg = palette.red[1], bg = "none" },
+			}
+
+			for group, styles in pairs(highlights_groups) do
+				vim.api.nvim_set_hl(0, group, styles)
+			end
+		end,
+	},
 	--
 	-- {
 	-- 	"ramojus/mellifluous.nvim",
-	-- 	config = function()
-	-- 		require("mellifluous").setup({ --[[...]]
-	-- 		}) -- optional, see configuration section.
-	-- 	end,
+	-- 	opts = {
+	-- 		styles = {
+	-- 			transparent_background = {
+	-- 				enabled = true,
+	-- 				floating_windows = true,
+	-- 				telescope = true,
+	-- 				file_tree = true,
+	-- 				cursor_line = true,
+	-- 				status_line = false,
+	-- 			},
+	-- 		},
+	-- 	},
 	-- },
+	-- { "savq/melange-nvim" },
+	-- { "nyoom-engineering/oxocarbon.nvim" },
 	-- {
 	-- 	"mcchrish/zenbones.nvim",
 	-- 	-- Optionally install Lush. Allows for more configuration or extending the colorscheme
@@ -49,22 +150,27 @@ return {
 	-- },
 	--
 	-- everforest
-	-- { "sainnhe/everforest" },
-	{
-		"neanias/everforest-nvim",
-		version = false,
-		lazy = false,
-		priority = 1000, -- make sure to load this before all the other start plugins
-		-- Optional; default configuration will be used if setup isn't called.
-		config = function()
-			require("everforest").setup({
-				-- Your config here
-				background = "hard",
-				italics = true,
-				transparent_background_level = 2,
-			})
-		end,
-	},
+	-- {
+	-- 	"sainnhe/everforest",
+	-- 	config = function()
+	-- 		vim.g.everforest_transparent_background = 2
+	-- 	end,
+	-- },
+	-- {
+	-- 	"neanias/everforest-nvim",
+	-- 	version = false,
+	-- 	lazy = false,
+	-- 	priority = 1000, -- make sure to load this before all the other start plugins
+	-- 	-- Optional; default configuration will be used if setup isn't called.
+	-- 	config = function()
+	-- 		require("everforest").setup({
+	-- 			-- Your config here
+	-- 			background = "hard",
+	-- 			italics = true,
+	-- 			transparent_background_level = 2,
+	-- 		})
+	-- 	end,
+	-- },
 
 	-- nightfox
 	-- {
@@ -95,6 +201,7 @@ return {
 	-- 	config = function()
 	-- 		require("github-theme").setup({
 	-- 			options = {
+	-- 				transparent = true,
 	-- 				styles = {
 	-- 					comments = "italic",
 	-- 					keywords = "bold",
@@ -125,26 +232,28 @@ return {
 	-- kanagawa
 	-- {
 	-- 	"rebelot/kanagawa.nvim",
-	-- 	overrides = function(colors)
-	-- 		local theme = colors.theme
-	-- 		return {
-	-- 			IndentBlanklineChar = { fg = theme.ui.bg_p1 },
-	-- 			IndentBlanklineContextChar = { fg = theme.ui.bg_p2 },
-	-- 			IndentBlanklineContextStart = { sp = theme.ui.bg_p2 },
-	-- 		}
-	-- 	end,
+	-- 	opts = {
+	-- 		overrides = function(colors)
+	-- 			local theme = colors.theme
+	-- 			return {
+	-- 				IndentBlanklineChar = { fg = theme.ui.bg_p1 },
+	-- 				IndentBlanklineContextChar = { fg = theme.ui.bg_p2 },
+	-- 				IndentBlanklineContextStart = { sp = theme.ui.bg_p2 },
+	-- 			}
+	-- 		end,
+	-- 	},
 	-- },
 	-- Configure LazyVim to load colorscheme
-	{
-		"LazyVim/LazyVim",
-		opts = function(_, opts)
-			opts.colorscheme = "everforest"
-			-- if os_is_dark() then
-			-- 	vim.o.background = "dark"
-			-- else
-			-- 	vim.o.background = "light"
-			-- end
-			-- return opts
-		end,
-	},
+	-- {
+	-- 	"LazyVim/LazyVim",
+	-- 	opts = function(_, opts)
+	-- 		opts.colorscheme = "gruvbox-material"
+	-- 		-- if os_is_dark() then
+	-- 		-- 	vim.o.background = "dark"
+	-- 		-- else
+	-- 		-- 	vim.o.background = "light"
+	-- 		-- end
+	-- 		-- return opts
+	-- 	end,
+	-- },
 }
