ae5a918841cee55dd98cf56f2f22149f7eb1a8a1
Akash <aakash@hey.com>
2023-12-02 22:21:23 +0530

    init neogit

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,5 +1,21 @@
 return {
 	{
-		"sindrets/diffview.nvim",
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"nvim-telescope/telescope.nvim", -- optional
+			"sindrets/diffview.nvim", -- optional
+		},
+		config = function()
+			require("neogit").setup({
+				integrations = {
+					telescope = true,
+					diffview = true,
+				},
+			})
+		end,
+		keys = {
+			{ "<leader>gn", "<cmd>Neogit<cr>", desc = "Neogit" },
+		},
 	},
 }
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -9,6 +9,7 @@ return {
 				["<leader><tab>"] = { name = "Buffer list (with preview)" },
 				["<leader>t"] = { name = "+tabs" },
 				["<leader>m"] = { name = "+harpoon" },
+				["<leader>gn"] = { name = "Neogit" },
 			},
 			triggers_blacklist = {
 				-- list of mode / prefixes that should never be hooked by WhichKey
