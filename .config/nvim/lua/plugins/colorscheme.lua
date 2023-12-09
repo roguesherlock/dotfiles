43eec39bb167eaa62e31445787763c96eddf4a54
Akash <aakash@hey.com>
2023-12-09 18:40:34 +0530

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -27,6 +27,7 @@ return {
 	-- 		vim.g.moonflyTransparent = true
 	-- 	end,
 	-- },
+	-- { "kepano/flexoki-neovim", name = "flexoki" },
 	-- {
 	-- 	"mikesmithgh/gruvsquirrel.nvim",
 	-- },
@@ -69,77 +70,77 @@ return {
 	-- 	},
 	-- },
 
-	-- {
-	-- 	"sainnhe/gruvbox-material",
-	-- 	name = "gruvbox-material",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		vim.g.gruvbox_material_better_performance = 1
-	-- 		-- Fonts
-	-- 		vim.g.gruvbox_material_disable_italic_comment = 1
-	-- 		vim.g.gruvbox_material_enable_italic = 0
-	-- 		vim.g.gruvbox_material_enable_bold = 0
-	-- 		vim.g.gruvbox_material_transparent_background = 1
-	-- 		-- Themes
-	-- 		vim.g.gruvbox_material_foreground = "mix"
-	-- 		vim.g.gruvbox_material_background = "hard"
-	-- 		vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
-	-- 		vim.g.gruvbox_material_float_style = "dim" -- Background of floating windows
-	--
-	-- 		local configuration = vim.fn["gruvbox_material#get_configuration"]()
-	-- 		local palette = vim.fn["gruvbox_material#get_palette"](
-	-- 			configuration.background,
-	-- 			configuration.foreground,
-	-- 			configuration.colors_override
-	-- 		)
-	--
-	-- 		-- vim.cmd.colorscheme("gruvbox-material")
-	--
-	-- 		local highlights_groups = {
-	-- 			FoldColumn = { bg = "none" },
-	-- 			SignColumn = { bg = "none" },
-	-- 			Normal = { bg = "none" },
-	-- 			NormalNC = { bg = "none" },
-	-- 			NormalFloat = { bg = "none" },
-	-- 			FloatBorder = { bg = "none" },
-	-- 			FloatTitle = { bg = "none", fg = palette.orange[1] },
-	-- 			TelescopeBorder = { bg = "none" },
-	-- 			TelescopeNormal = { fg = "none" },
-	-- 			TelescopePromptNormal = { bg = "none" },
-	-- 			TelescopeResultsNormal = { bg = "none" },
-	-- 			TelescopeSelection = { bg = palette.bg3[1] },
-	-- 			Visual = { bg = palette.bg_visual_red[1] },
-	-- 			Cursor = { bg = palette.bg_red[1], fg = palette.bg_dim[1] },
-	-- 			ColorColumn = { bg = palette.bg_visual_blue[1] },
-	-- 			CursorLine = { bg = palette.bg3[1], blend = 25 },
-	-- 			GitSignsAdd = { fg = palette.green[1], bg = "none" },
-	-- 			GitSignsChange = { fg = palette.yellow[1], bg = "none" },
-	-- 			GitSignsDelete = { fg = palette.red[1], bg = "none" },
-	-- 		}
-	--
-	-- 		for group, styles in pairs(highlights_groups) do
-	-- 			vim.api.nvim_set_hl(0, group, styles)
-	-- 		end
-	-- 	end,
-	-- },
-	--
 	{
-		"ramojus/mellifluous.nvim",
+		"sainnhe/gruvbox-material",
+		name = "gruvbox-material",
 		lazy = false,
 		priority = 1000,
-		opts = {
-			color_set = "mountain",
-			transparent_background = {
-				enabled = true,
-				floating_windows = true,
-				telescope = true,
-				file_tree = true,
-				cursor_line = true,
-				status_line = false,
-			},
-		},
+		config = function()
+			vim.g.gruvbox_material_better_performance = 1
+			-- Fonts
+			vim.g.gruvbox_material_disable_italic_comment = 1
+			vim.g.gruvbox_material_enable_italic = 0
+			vim.g.gruvbox_material_enable_bold = 0
+			vim.g.gruvbox_material_transparent_background = 1
+			-- Themes
+			vim.g.gruvbox_material_foreground = "mix"
+			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
+			vim.g.gruvbox_material_float_style = "dim" -- Background of floating windows
+
+			local configuration = vim.fn["gruvbox_material#get_configuration"]()
+			local palette = vim.fn["gruvbox_material#get_palette"](
+				configuration.background,
+				configuration.foreground,
+				configuration.colors_override
+			)
+
+			-- vim.cmd.colorscheme("gruvbox-material")
+
+			local highlights_groups = {
+				FoldColumn = { bg = "none" },
+				SignColumn = { bg = "none" },
+				Normal = { bg = "none" },
+				NormalNC = { bg = "none" },
+				NormalFloat = { bg = "none" },
+				FloatBorder = { bg = "none" },
+				FloatTitle = { bg = "none", fg = palette.orange[1] },
+				TelescopeBorder = { bg = "none" },
+				TelescopeNormal = { fg = "none" },
+				TelescopePromptNormal = { bg = "none" },
+				TelescopeResultsNormal = { bg = "none" },
+				TelescopeSelection = { bg = palette.bg3[1] },
+				Visual = { bg = palette.bg_visual_red[1] },
+				Cursor = { bg = palette.bg_red[1], fg = palette.bg_dim[1] },
+				ColorColumn = { bg = palette.bg_visual_blue[1] },
+				CursorLine = { bg = palette.bg3[1], blend = 25 },
+				GitSignsAdd = { fg = palette.green[1], bg = "none" },
+				GitSignsChange = { fg = palette.yellow[1], bg = "none" },
+				GitSignsDelete = { fg = palette.red[1], bg = "none" },
+			}
+
+			for group, styles in pairs(highlights_groups) do
+				vim.api.nvim_set_hl(0, group, styles)
+			end
+		end,
 	},
+
+	-- {
+	-- 	"ramojus/mellifluous.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {
+	-- 		color_set = "mountain",
+	-- 		transparent_background = {
+	-- 			enabled = true,
+	-- 			floating_windows = true,
+	-- 			telescope = true,
+	-- 			file_tree = true,
+	-- 			cursor_line = true,
+	-- 			status_line = false,
+	-- 		},
+	-- 	},
+	-- },
 	-- { "savq/melange-nvim" },
 	-- { "nyoom-engineering/oxocarbon.nvim" },
 	-- {
@@ -259,8 +260,8 @@ return {
 	{
 		"LazyVim/LazyVim",
 		opts = function(_, opts)
-			-- opts.colorscheme = "gruvbox-material"
-			opts.colorscheme = "mellifluous"
+			opts.colorscheme = "gruvbox-material"
+			-- opts.colorscheme = "mellifluous"
 			-- if os_is_dark() then
 			-- 	vim.o.background = "dark"
 			-- else
