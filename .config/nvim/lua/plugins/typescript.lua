75d0e0f4a7d5291e5236a334144bb85d4ee38a68
Akash <aakash@hey.com>
2023-12-25 01:51:41 +0530

    update typescript

--- .config/nvim/lua/plugins/typescript.lua
+++ .config/nvim/lua/plugins/typescript.lua
@@ -9,6 +9,21 @@ return {
 	},
 	{
 		"pmizio/typescript-tools.nvim",
-		opts = {},
+		opts = {
+			expose_as_code_action = "all",
+			on_attach = function(client, bufnr)
+				client.server_capabilities.documentFormattingProvider = false
+				client.server_capabilities.documentRangeFormattingProvider = false
+			end,
+			settings = {
+				separate_diagnostic_server = true,
+				composite_mode = "separate_diagnostic",
+				publish_diagnostic_on = "insert_leave",
+				-- tsserver_logs = "verbose",
+				tsserver_file_preferences = {
+					importModuleSpecifierPreference = "non-relative",
+				},
+			},
+		},
 	},
 }
