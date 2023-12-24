5ff24f98c9df8f9af0183d77c756665e1b6dccb1
Akash <aakash@hey.com>
2023-12-25 01:51:44 +0530

    try cody

---
+++ .config/nvim/lua/plugins/cody.lua
@@ -0,0 +1,9 @@
+return {
+	"sourcegraph/sg.nvim",
+	event = "InsertEnter",
+	cmd = { "CodyAsk", "CodyChat", "SourcegraphLink", "SourcegraphLogin" },
+	keys = {
+		{ "<D-i>", "<cmd>CodyToggle<cr>", desc = "Toggle Cody Chat" },
+	},
+	opts = {},
+}
