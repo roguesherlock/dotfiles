25c10263ec6907eb036afac15f5e19df2ec345d2
Akash <aakash@hey.com>
2023-12-31 14:02:33 +0530

    init kitty scrollback

---
+++ .config/nvim/lua/plugins/kitty-scrollback.lua
@@ -0,0 +1,13 @@
+return {
+	"mikesmithgh/kitty-scrollback.nvim",
+	enabled = true,
+	lazy = true,
+	cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
+	event = { "User KittyScrollbackLaunch" },
+	-- version = '*', -- latest stable version, may have breaking changes if major version changed
+	-- version = '^2.0.0', -- pin major version, include fixes and features that do not have breaking changes
+	-- config = function()
+	-- 	require("kitty-scrollback").setup()
+	-- end,
+	opts = {},
+}
