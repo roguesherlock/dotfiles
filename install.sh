51e2fbfd444e37468a7e983be944e686eccca9b4
Akash <aakash@hey.com>
2024-03-07 10:24:06 +0530

    update install script

--- install.sh
+++ install.sh
@@ -4,6 +4,16 @@ mkdir ~/Developer
 
 OS=$(uname -s)
 DOTFILES_DIR=~/Developer/dotfiles
+FORCE=false
+
+# Parse named command-line arguments
+while [[ "$#" -gt 0 ]]; do
+	case $1 in
+	--force) FORCE=true ;;
+	*) break ;;
+	esac
+	shift
+done
 
 isDarwin() {
 	[ "$OS" = "Darwin" ]
@@ -13,65 +23,89 @@ isLinux() {
 	[ "$OS" = "Linux" ]
 }
 
+install() {
+	local TARGET_PATH="$1"
+	local LINK_PATH="$2"
+
+	if [ "$FORCE" = "true" ]; then
+		# Create the symbolic link
+		ln -sf "$TARGET_PATH" "$LINK_PATH"
+		echo "Symbolic link forcefully created at $LINK_PATH -> $TARGET_PATH"
+
+		# Check if the symlink or file already exists at the destination
+	elif [ -e "$LINK_PATH" ] || [ -L "$LINK_PATH" ]; then
+		echo "[warn] A file or symbolic link already exists at $LINK_PATH. No action taken."
+	else
+		# Create the symbolic link
+		ln -s "$TARGET_PATH" "$LINK_PATH"
+		echo "Symbolic link created at $LINK_PATH -> $TARGET_PATH"
+	fi
+}
+
 if isDarwin; then
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  if ! command -v brew &> /dev/null; then
+    echo "Homebrew not found. Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  fi
 fi
 
 if [ -d "$DOTFILES_DIR" ]; then
-	git clone https://github.com/roguesherlock/dotfiles.git $DOTFILES_DIR
 	echo "[warn] Looks like dotfiles already exists. Not cloning again."
+else
+	git clone https://github.com/roguesherlock/dotfiles.git $DOTFILES_DIR
+  echo "Dotfiles cloned to $DOTFILES_DIR"
 fi
 
 #fish
 mkdir -p ~/.config/fish/
-ln -sF ~/Developer/dotfiles/.config/fish ~/.config/fish
+install ~/Developer/dotfiles/.config/fish ~/.config/fish
 
 # tmux
-ln -sf ~/Developer/dotfiles/.tmux.conf ~/.tmux.conf
+install ~/Developer/dotfiles/.tmux.conf ~/.tmux.conf
 
 # vimrc
-ln -sf ~/Developer/dotfiles/.vimrc ~/.vimrc
+install ~/Developer/dotfiles/.vimrc ~/.vimrc
 
 # ssh
 mkdir -p ~/.ssh
-ln -sf ~/Developer/dotfiles/.ssh/config ~/.ssh/config
+install ~/Developer/dotfiles/.ssh/config ~/.ssh/config
 
 # gnupg
 mkdir -p ~/.gnupg
-ln -sf ~/Developer/dotfiles/.gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
-ln -sf ~/Developer/dotfiles/.gnupg/gpg.conf ~/.gnupg/gpg.conf
+install ~/Developer/dotfiles/.gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+install ~/Developer/dotfiles/.gnupg/gpg.conf ~/.gnupg/gpg.conf
 
 # git
-ln -sf ~/Developer/dotfiles/.gitconfig ~/.gitconfig
+install ~/Developer/dotfiles/.gitconfig ~/.gitconfig
 
 # alacritty
 mkdir -p ~/.config/alacritty/
-ln -sF ~/Developer/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+install ~/Developer/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 if isDarwin; then
 	#karabinner
-	ln -sF ~/Developer/dotfiles/.config/karabiner ~/.config/karabiner
+	install ~/Developer/dotfiles/.config/karabiner ~/.config/karabiner
 fi
 
 #nvim
-ln -sF ~/Developer/dotfiles/.config/nvim ~/.config/nvim
+install ~/Developer/dotfiles/.config/nvim ~/.config/nvim
 
 #wezterm
-ln -sF ~/Developer/dotfiles/.config/wezterm ~/.config/wezterm
+install ~/Developer/dotfiles/.config/wezterm ~/.config/wezterm
 
 #kitty
-ln -sF ~/Developer/dotfiles/.config/kitty ~/.config/kitty
+install ~/Developer/dotfiles/.config/kitty ~/.config/kitty
 
 #lazygit
-ln -sF ~/Developer/dotfiles/.config/lazygit ~/.config/lazygit
+install ~/Developer/dotfiles/.config/lazygit ~/.config/lazygit
 if isDarwin; then
 	mkdir -p ~/Library/Application\ Support/lazygit
-	ln -sF ~/Developer/dotfiles/.config/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
+	install ~/Developer/dotfiles/.config/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
 fi
 
 # mise
 mkdir -p ~/.config/mise/
-ln -sF ~/Developer/dotfiles/.config/mise ~/.config/mise
+install ~/Developer/dotfiles/.config/mise ~/.config/mise
 
 # startship prompt
-ln -sF ~/Developer/dotfiles/.config/starship.toml ~/.config/starship.toml
+install ~/Developer/dotfiles/.config/starship.toml ~/.config/starship.toml
