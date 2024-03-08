5261dacec713c84261dc11e124e298612dcc8847
Akash <aakash@hey.com>
2024-03-08 17:56:43 +0530

    fix script

--- install-nixos.sh
+++ install-nixos.sh
@@ -4,9 +4,9 @@
 #
 
 if [ $# -eq 0 ]; then
-    echo "Usage: $0 <machine-name>"
-    echo "Please provide the name of the machine you want to install as an argument.
-    exit 1
+	echo "Usage: $0 <machine-name>"
+	echo "Please provide the name of the machine you want to install as an argument."
+	exit 1
 fi
 
 mkdir ~/Developer
@@ -15,7 +15,6 @@ mkdir ~/Developer
 nix-shell -p git --command "git clone https://github.com/roguesherlock/dotfiles ~/Developer/dotfiles"
 
 # Generate hardware config for new system
-sudo nixos-generate-config --show-hardware-config > ~/Developer/dotfiles/nixos/machines/hardware-configuration.nix
+sudo nixos-generate-config --show-hardware-config >~/Developer/dotfiles/nixos/machines/hardware-configuration.nix
 
-sudo nixos-rebuild switch --flake ~/Developer/dotfiles#$1;
-
+sudo nixos-rebuild switch --flake ~/Developer/dotfiles#$1
