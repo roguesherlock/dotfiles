7acd20f94590eb71f20f1e059024ec1041a489cc
Akash <aakash@hey.com>
2024-07-03 18:54:22 +0530

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -306,6 +306,16 @@ return {
 			-- variant = "deuteranopia",
 		},
 	},
+	{
+		"folke/tokyonight.nvim",
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
 	-- {
 	-- 	"craftzdog/solarized-osaka.nvim",
 	-- 	branch = "osaka",
