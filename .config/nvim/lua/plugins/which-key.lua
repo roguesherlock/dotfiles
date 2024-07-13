fabe6967627e3074c197775890924d3b89af7b72
Akash <aakash@hey.com>
2024-07-13 14:16:24 +0530

    update

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,14 +2,14 @@ return {
 	{
 		"folke/which-key.nvim",
 		opts = {
-			defaults = {
+			spec = {
 				-- ["<leader>z"] = { name = "Zen" },
 				-- ["<leader>j"] = { name = "Find Files (Git)" },
 				-- ["<leader>sl"] = { name = "Live Grep (with args)" },
 				-- ["<leader><tab>"] = { name = "Buffer list (with preview)" },
-				["<leader>t"] = { name = "+tabs,+toggle" },
-				["<leader>m"] = { name = "+marks" },
-				["<leader>gn"] = { name = "Neogit" },
+				-- ["<leader>t"] = { name = "+tabs,+toggle" },
+				-- ["<leader>m"] = { name = "+marks" },
+				-- ["<leader>gn"] = { name = "Neogit" },
 			},
 			triggers_blacklist = {
 				-- list of mode / prefixes that should never be hooked by WhichKey
