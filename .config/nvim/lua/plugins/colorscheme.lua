1bae8817ce40d4083b7b2309ab1cd259539e59fc
Akash <aakash@hey.com>
2024-07-19 12:50:14 +0530

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,20 @@ return {
 		priority = 1000,
 	},
 	-- {
+	-- 	"0xstepit/flow.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {},
+	-- 	config = function()
+	-- 		require("flow").setup_options({
+	-- 			transparent = true, -- Set transparent background.
+	-- 			fluo_color = "pink", --  Fluo color: pink, yellow, orange, or green.
+	-- 			mode = "normal", -- Intensity of the palette: normal, dark, or bright. Notice that dark is ugly!
+	-- 			aggressive_spell = false, -- Display colors for spell check.
+	-- 		})
+	-- 	end,
+	-- },
+	-- {
 	-- 	"bluz71/vim-nightfly-guicolors",
 	-- 	lazy = true,
 	-- 	priority = 1000, -- make sure to load this before all the other start plugins
