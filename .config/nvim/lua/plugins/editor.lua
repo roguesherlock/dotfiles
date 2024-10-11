f1388dc397c18cc67e0ad95503c8d3564fb35f2f
Akash <aakash@hey.com>
2024-10-11 10:51:03 +0530

    blade template

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -89,4 +89,31 @@ return {
 			},
 		},
 	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"blade",
+			},
+		},
+		config = function(_, opts)
+			-- Blade --
+			-- Filetypes --
+			vim.filetype.add({
+				pattern = {
+					[".*%.blade%.php"] = "blade",
+				},
+			})
+			require("nvim-treesitter.configs").setup(opts)
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			parser_config.blade = {
+				install_info = {
+					url = "https://github.com/EmranMR/tree-sitter-blade",
+					files = { "src/parser.c" },
+					branch = "main",
+				},
+				filetype = "blade",
+			}
+		end,
+	},
 }
