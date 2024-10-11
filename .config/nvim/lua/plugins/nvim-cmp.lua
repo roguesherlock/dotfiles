205346e42349c63f5aa5b00135cdf59a0f661e2a
Akash <aakash@hey.com>
2024-10-11 10:51:25 +0530

    blink

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"hrsh7th/nvim-cmp",
+		-- enabled = false,
 		keys = {
 			{ "<tab>", false, mode = { "i", "s" } },
 			{ "<s-tab>", false, mode = { "i", "s" } },
@@ -69,5 +70,59 @@ return {
 				end, { "i", "s" }),
 			})
 		end,
+	},
+	{
+		"saghen/blink.cmp",
+		enabled = false,
+		lazy = false, -- lazy loading handled internally
+		-- optional: provides snippets for the snippet source
+		dependencies = "rafamadriz/friendly-snippets",
+		-- use a release tag to download pre-built binaries
+		version = "v0.*",
+		-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+		opts = {
+			keymap = {
+				show = "<C-space>",
+				hide = "<C-e>",
+				accept = "<C-y>",
+				-- accept = "<Cr>",
+				select_prev = { "<Up>", "<C-k>" },
+				select_next = { "<Down>", "<C-j>" },
+				show_documentation = {},
+				hide_documentation = {},
+				scroll_documentation_up = "<C-p>",
+				scroll_documentation_down = "<C-n>",
+				-- Think of <c-l> as moving to the right of your snippet expansion.
+				--  So if you have a snippet that's like:
+				--  function $name($args)
+				--    $body
+				--  end
+				--
+				-- <c-l> will move you to the right of each of the expansion locations.
+				-- <c-h> is similar, except moving you backwards.
+				snippet_forward = "<C-l>",
+				snippet_backward = "<C-h>",
+			},
+			-- windows = {
+			-- 	autocomplete = {
+			-- 		border = "rounded",
+			-- 	},
+			-- },
+			highlight = {
+				-- sets the fallback highlight groups to nvim-cmp's highlight groups
+				-- useful for when your theme doesn't support blink.cmp
+				-- will be removed in a future release, assuming themes add support
+				-- use_nvim_cmp_as_default = true,
+			},
+			-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "normal",
+			-- experimental auto-brackets support
+			-- accept = { auto_brackets = { enabled = true } }
+
+			-- experimental signature help support
+			trigger = { signature_help = { enabled = true } },
+		},
 	},
 }
