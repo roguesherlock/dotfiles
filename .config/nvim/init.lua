3d4a90e7b1410ea3bb864d076f938668ba3e8dcd
Akash <aakash@hey.com>
2024-10-16 13:03:58 +0530

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -197,6 +197,26 @@ local function setup_autocommands()
       vim.cmd [[ silent! update ]]
     end,
   })
+
+  -- Close certain filetypes with 'q'
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = {
+      'qf',           -- quickfix list
+      'help',         -- help files
+      'man',          -- man pages
+      'notify',       -- notifications
+      'lspinfo',      -- lsp info
+      'spectre_panel',
+      'startuptime',
+      'tsplayground',
+      'PlenaryTestPopup',
+      'mini.pick',    -- mini.pick
+    },
+    callback = function(event)
+      vim.bo[event.buf].buflisted = false
+      vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
+    end,
+  })
 end
 
 local function setup_plugin_manager()
