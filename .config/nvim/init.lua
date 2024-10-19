f964c9cdd918dd5bddc16258e80cf80dc06911c7
Akash <aakash@hey.com>
2024-10-19 09:50:46 +0530

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1931,13 +1931,98 @@ local function markdown()
   add 'OXY2DEV/markview.nvim'
 end
 
+local function dap()
+  add {
+    source = 'mfussenegger/nvim-dap',
+    depends = {
+      -- Creates a beautiful debugger UI
+      'rcarriga/nvim-dap-ui',
+
+      -- Required dependency for nvim-dap-ui
+      'nvim-neotest/nvim-nio',
+
+      -- Installs the debug adapters for you
+      'williamboman/mason.nvim',
+      'jay-babu/mason-nvim-dap.nvim',
+
+      -- Add your own debuggers here
+      'leoluz/nvim-dap-go',
+    },
+  }
+  local dap = require 'dap'
+  local dapui = require 'dapui'
+
+  require('mason-nvim-dap').setup {
+    -- Makes a best effort to setup the various debuggers with
+    -- reasonable debug configurations
+    automatic_installation = true,
+
+    -- You can provide additional configuration to the handlers,
+    -- see mason-nvim-dap README for more information
+    handlers = {},
+
+    -- You'll need to check that you have the required things installed
+    -- online, please don't ask me how to install them :)
+    ensure_installed = {
+      -- Update this to ensure that you have the debuggers for the langs you want
+      'delve',
+    },
+  }
+
+  -- Dap UI setup
+  -- For more information, see |:help nvim-dap-ui|
+  dapui.setup {
+    -- Set icons to characters that are more likely to work in every terminal.
+    --    Feel free to remove or use ones that you like more! :)
+    --    Don't feel like these are good choices.
+    icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+    controls = {
+      icons = {
+        pause = '⏸',
+        play = '▶',
+        step_into = '⏎',
+        step_over = '⏭',
+        step_out = '⏮',
+        step_back = 'b',
+        run_last = '▶▶',
+        terminate = '⏹',
+        disconnect = '⏏',
+      },
+    },
+  }
+
+  dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+  dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+  dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+  -- Install golang specific config
+  require('dap-go').setup {
+    delve = {
+      -- On Windows delve must be run attached or it crashes.
+      -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+      detached = vim.fn.has 'win32' == 0,
+    },
+  }
+  -- mappings
+  map('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+  map('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+  map('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+  map('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+  map('n', '<leader>tdb', dap.toggle_breakpoint, { desc = '[T]oggle [D]ebug [B]reakpoint' })
+  map('n', '<leader>tdB', function()
+    dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+  end, { desc = '[T]oggle [D]ebug [B]reakpoint with condition' })
+  -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+  map('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+end
+
 -- Lazy load plugins
 local function setup_plugins()
   noice()
   plugins_that_should_be_the_default()
   which_key()
   -- auto_session()
-  auto_session_custom()
+  -- auto_session_custom()
   -- telescope()
   grug()
   git()
@@ -1960,6 +2045,7 @@ local function setup_plugins()
   nvim_ufo()
   overseer()
   markdown()
+  dap()
 end
 
 setup_plugin_manager()
