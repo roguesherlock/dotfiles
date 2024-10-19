59e8739fe3c44d15f013604d476fc517de6df83c
Akash <aakash@hey.com>
2024-10-19 10:14:49 +0530

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2016,6 +2016,56 @@ local function dap()
   map('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
 end
 
+local function lint()
+  add 'mfussenegger/nvim-lint'
+  local lint = require 'lint'
+  lint.linters_by_ft = {
+    markdown = { 'markdownlint' },
+  }
+
+  -- To allow other plugins to add linters to require('lint').linters_by_ft,
+  -- instead set linters_by_ft like this:
+  -- lint.linters_by_ft = lint.linters_by_ft or {}
+  -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+  --
+  -- However, note that this will enable a set of default linters,
+  -- which will cause errors unless these tools are available:
+  -- {
+  --   clojure = { "clj-kondo" },
+  --   dockerfile = { "hadolint" },
+  --   inko = { "inko" },
+  --   janet = { "janet" },
+  --   json = { "jsonlint" },
+  --   markdown = { "vale" },
+  --   rst = { "vale" },
+  --   ruby = { "ruby" },
+  --   terraform = { "tflint" },
+  --   text = { "vale" }
+  -- }
+  --
+  -- You can disable the default linters by setting their filetypes to nil:
+  -- lint.linters_by_ft['clojure'] = nil
+  -- lint.linters_by_ft['dockerfile'] = nil
+  -- lint.linters_by_ft['inko'] = nil
+  -- lint.linters_by_ft['janet'] = nil
+  -- lint.linters_by_ft['json'] = nil
+  -- lint.linters_by_ft['markdown'] = nil
+  -- lint.linters_by_ft['rst'] = nil
+  -- lint.linters_by_ft['ruby'] = nil
+  -- lint.linters_by_ft['terraform'] = nil
+  -- lint.linters_by_ft['text'] = nil
+
+  -- Create autocommand which carries out the actual linting
+  -- on the specified events.
+  local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+  vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+    group = lint_augroup,
+    callback = function()
+      lint.try_lint()
+    end,
+  })
+end
+
 -- Lazy load plugins
 local function setup_plugins()
   noice()
@@ -2046,6 +2096,7 @@ local function setup_plugins()
   overseer()
   markdown()
   dap()
+  lint()
 end
 
 setup_plugin_manager()
