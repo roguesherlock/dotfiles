0ae0f8d1912b510906b58112351b9546936ca57e
Akash <aakash@hey.com>
2024-11-25 11:57:07 +0530

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1719,14 +1719,15 @@ local function lsp()
     vim.diagnostic.config {
       underline = true,
       update_in_insert = false,
-      virtual_text = {
-        spacing = 4,
-        source = 'if_many',
-        -- prefix = '●',
-        -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
-        -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
-        -- prefix = 'icons',
-      },
+      virtual_text = false,
+      -- virtual_text = {
+      --   spacing = 4,
+      --   source = 'if_many',
+      --   -- prefix = '●',
+      --   -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+      --   -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+      --   -- prefix = 'icons',
+      -- },
       severity_sort = true,
       -- signs = {
       --   text = {
