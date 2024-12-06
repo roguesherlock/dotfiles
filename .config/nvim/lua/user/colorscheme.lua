536908f6e8696089ed32772370d51d84bec0eaa5
Akash <aakash@hey.com>
2024-12-06 23:23:46 +0530

    update

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -68,6 +68,21 @@ function M.set_colorscheme(light)
   if M.config.opts.set_theme_on_auto_switch then
     vim.cmd("colorscheme " .. colorscheme)
   end
+  vim.defer_fn(function()
+    if light then
+      vim.fn.system("kitty +kitten themes " .. M.config.kitty.light)
+      M.set_ghostty_theme(M.config.ghostty.light, M.config.ghostty.custom_theme)
+      M.set_zellij_theme(M.config.zellij.light)
+      M.set_delta_theme(M.config.delta.light)
+      M.set_yazi_theme(M.config.yazi.light)
+    else
+      vim.fn.system("kitty +kitten themes " .. M.config.kitty.dark)
+      M.set_ghostty_theme(M.config.ghostty.dark, M.config.ghostty.custom_theme)
+      M.set_zellij_theme(M.config.zellij.dark)
+      M.set_delta_theme(M.config.delta.dark)
+      M.set_yazi_theme(M.config.yazi.dark)
+    end
+  end, 0)
 end
 
 function M.set_from_os()
@@ -103,115 +118,6 @@ function M.get_colorscheme()
   -- end
 end
 
-local function switch_theme(theme)
-  local theme_name, theme_type = theme:match("([^_]*)_([^_]*)")
-  if theme_type == "light" then
-    theme_type = "dark"
-  else
-    theme_type = "light"
-  end
-  return theme_name .. "_" .. theme_type
-end
-
-local function set_ghostty_theme(theme, is_custom_theme)
-  is_custom_theme = is_custom_theme or false
-  if not is_custom_theme then
-    -- vim.fn.system("sed -i'.bak' 's/theme = .*/theme = " .. ghostty_light_theme .. "/' (readlink ~/.config/ghostty/config)")
-    local config_path = vim.fn.expand("~/.config/ghostty/config")
-    local real_path = vim.fn.resolve(config_path)
-    local cmd = string.format("sed -i'.bak' 's/theme = .*/theme = %s/' %s", theme, real_path)
-    local result = vim.fn.system({ "bash", "-c", cmd })
-    if vim.v.shell_error ~= 0 then
-      print("Error updating Ghostty theme: " .. result)
-    end
-    return
-  end
-  local base_config_path = vim.fn.expand("~/.config/ghostty/config")
-  local theme_file_path = vim.fn.expand("~/.config/ghostty/" .. theme .. ".conf")
-
-  -- Check if the theme file exists
-  if vim.fn.filereadable(theme_file_path) == 0 then
-    print("Theme file not found: " .. theme_file_path)
-    return
-  end
-
-  local currentTheme = switch_theme(theme)
-
-  -- Read the content of the theme file
-  local theme_content = vim.fn.readfile(theme_file_path)
-
-  -- Read the current content of the base config file
-  local base_config_content = vim.fn.readfile(base_config_path)
-
-  -- Find the start and end indices of the current theme section
-  local start_index, end_index
-  local in_theme_section = false
-  for i, line in ipairs(base_config_content) do
-    if line:match("^# %s*" .. theme .. "$") then
-      return
-    elseif line:match("^# %s*" .. currentTheme .. "$") then
-      start_index = i
-      in_theme_section = true
-    elseif line:match("^# End*$") and in_theme_section then
-      end_index = i
-      break
-    end
-  end
-
-  -- Remove the current theme section if found
-  if start_index and end_index then
-    for i = end_index, start_index, -1 do
-      table.remove(base_config_content, i)
-    end
-  end
-
-  -- Insert the new theme content at the position where the old theme was removed
-  -- or at the end if no theme section was found
-  local insert_position = start_index or (#base_config_content + 1)
-  for i, line in ipairs(theme_content) do
-    table.insert(base_config_content, insert_position, line)
-    insert_position = insert_position + 1
-  end
-
-  -- Write the updated content back to the base config file
-  vim.fn.writefile(base_config_content, base_config_path)
-
-  -- print("Theme set to: " .. theme)
-end
-
-local function set_zellij_theme(theme)
-  local config_path = vim.fn.expand("~/.config/zellij/config.kdl")
-  local real_path = vim.fn.resolve(config_path)
-  local cmd = string.format('sed -i\'.bak\' \'s/theme "[^"]*"/theme "%s"/\' %s', theme, real_path)
-  local result = vim.fn.system({ "bash", "-c", cmd })
-  if vim.v.shell_error ~= 0 then
-    print("Error updating Zellij theme: " .. result)
-  end
-  return
-end
-
-local function set_delta_theme(theme)
-  local config_path = vim.fn.expand("~/.gitconfig")
-  local real_path = vim.fn.resolve(config_path)
-  local cmd = string.format("sed -i'.bak' 's/features = .*/features = %s/' %s", theme, real_path)
-
-  local result = vim.fn.system({ "bash", "-c", cmd })
-  if vim.v.shell_error ~= 0 then
-    print("Error updating Delta theme for git: " .. result)
-  end
-end
-
-local function set_yazi_theme(theme)
-  local config_path = vim.fn.expand("~/.config/yazi/theme.toml")
-  local real_path = vim.fn.resolve(config_path)
-  local cmd = string.format('sed -i\'.bak\' \'s/use = "[^"]*"/use = "%s"/\' %s', theme, real_path)
-
-  local result = vim.fn.system({ "bash", "-c", cmd })
-  if vim.v.shell_error ~= 0 then
-    print("Error updating Yazi theme for git: " .. result)
-  end
-end
-
 function M.setup(config)
   -- Validate config is a table
   vim.validate({ config = { config, "table", true } })
@@ -225,29 +131,14 @@ function M.setup(config)
   vim.api.nvim_create_autocmd("Signal", {
     pattern = "*",
     callback = function()
+      print("Signal detected")
       M.set_from_os()
     end,
   })
 
   vim.api.nvim_create_autocmd("ColorScheme", {
     pattern = "*",
-    callback = function()
-      vim.defer_fn(function()
-        if vim.o.background == "light" then
-          vim.fn.system("kitty +kitten themes " .. M.config.kitty.light)
-          set_ghostty_theme(M.config.ghostty.light, M.config.ghostty.custom_theme)
-          set_zellij_theme(M.config.zellij.light)
-          set_delta_theme(M.config.delta.light)
-          set_yazi_theme(M.config.yazi.light)
-        else
-          vim.fn.system("kitty +kitten themes " .. M.config.kitty.dark)
-          set_ghostty_theme(M.config.ghostty.dark, M.config.ghostty.custom_theme)
-          set_zellij_theme(M.config.zellij.dark)
-          set_delta_theme(M.config.delta.dark)
-          set_yazi_theme(M.config.yazi.dark)
-        end
-      end, 0)
-    end,
+    callback = function() end,
   })
 
   vim.api.nvim_create_user_command("Light", function()
@@ -256,6 +147,113 @@ function M.setup(config)
   vim.api.nvim_create_user_command("Dark", function()
     M.set_colorscheme(false)
   end, {})
+end
+
+function M.set_ghostty_theme(theme, is_custom_theme)
+  is_custom_theme = is_custom_theme or false
+  if not is_custom_theme then
+    -- vim.fn.system("sed -i'.bak' 's/theme = .*/theme = " .. ghostty_light_theme .. "/' (readlink ~/.config/ghostty/config)")
+    local config_path = vim.fn.expand("~/.config/ghostty/config")
+    local real_path = vim.fn.resolve(config_path)
+    local cmd = string.format("sed -i'.bak' 's/theme = .*/theme = %s/' %s", theme, real_path)
+    local result = vim.fn.system({ "bash", "-c", cmd })
+    if vim.v.shell_error ~= 0 then
+      print("Error updating Ghostty theme: " .. result)
+    end
+    return
+  end
+  -- local function switch__theme(theme)
+  --   local theme_name, theme_type = theme:match("([^_]*)_([^_]*)")
+  --   if theme_type == "light" then
+  --     theme_type = "dark"
+  --   else
+  --     theme_type = "light"
+  --   end
+  --   return theme_name .. "_" .. theme_type
+  -- end
+  -- local base_config_path = vim.fn.expand("~/.config/ghostty/config")
+  -- local theme_file_path = vim.fn.expand("~/.config/ghostty/" .. theme .. ".conf")
+  --
+  -- -- Check if the theme file exists
+  -- if vim.fn.filereadable(theme_file_path) == 0 then
+  --   print("Theme file not found: " .. theme_file_path)
+  --   return
+  -- end
+  --
+  -- local currentTheme = switch_theme(theme)
+  --
+  -- -- Read the content of the theme file
+  -- local theme_content = vim.fn.readfile(theme_file_path)
+  --
+  -- -- Read the current content of the base config file
+  -- local base_config_content = vim.fn.readfile(base_config_path)
+  --
+  -- -- Find the start and end indices of the current theme section
+  -- local start_index, end_index
+  -- local in_theme_section = false
+  -- for i, line in ipairs(base_config_content) do
+  --   if line:match("^# %s*" .. theme .. "$") then
+  --     return
+  --   elseif line:match("^# %s*" .. currentTheme .. "$") then
+  --     start_index = i
+  --     in_theme_section = true
+  --   elseif line:match("^# End*$") and in_theme_section then
+  --     end_index = i
+  --     break
+  --   end
+  -- end
+  --
+  -- -- Remove the current theme section if found
+  -- if start_index and end_index then
+  --   for i = end_index, start_index, -1 do
+  --     table.remove(base_config_content, i)
+  --   end
+  -- end
+  --
+  -- -- Insert the new theme content at the position where the old theme was removed
+  -- -- or at the end if no theme section was found
+  -- local insert_position = start_index or (#base_config_content + 1)
+  -- for i, line in ipairs(theme_content) do
+  --   table.insert(base_config_content, insert_position, line)
+  --   insert_position = insert_position + 1
+  -- end
+  --
+  -- -- Write the updated content back to the base config file
+  -- vim.fn.writefile(base_config_content, base_config_path)
+  --
+  -- -- print("Theme set to: " .. theme)
+end
+
+function M.set_zellij_theme(theme)
+  local config_path = vim.fn.expand("~/.config/zellij/config.kdl")
+  local real_path = vim.fn.resolve(config_path)
+  local cmd = string.format('sed -i\'.bak\' \'s/theme "[^"]*"/theme "%s"/\' %s', theme, real_path)
+  local result = vim.fn.system({ "bash", "-c", cmd })
+  if vim.v.shell_error ~= 0 then
+    print("Error updating Zellij theme: " .. result)
+  end
+end
+
+function M.set_delta_theme(theme)
+  local config_path = vim.fn.expand("~/.gitconfig")
+  local real_path = vim.fn.resolve(config_path)
+  local cmd = string.format("sed -i'.bak' 's/features = .*/features = %s/' %s", theme, real_path)
+
+  local result = vim.fn.system({ "bash", "-c", cmd })
+  if vim.v.shell_error ~= 0 then
+    print("Error updating Delta theme for git: " .. result)
+  end
+end
+
+function M.set_yazi_theme(theme)
+  local config_path = vim.fn.expand("~/.config/yazi/theme.toml")
+  local real_path = vim.fn.resolve(config_path)
+  local cmd = string.format('sed -i\'.bak\' \'s/use = "[^"]*"/use = "%s"/\' %s', theme, real_path)
+
+  local result = vim.fn.system({ "bash", "-c", cmd })
+  if vim.v.shell_error ~= 0 then
+    print("Error updating Yazi theme for git: " .. result)
+  end
 end
 
 return M
