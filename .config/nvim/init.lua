bfc99d6abf3508ba3b8d0dc2f264ef8e10cb7123
Akash <aakash@hey.com>
2024-12-06 23:35:33 +0530

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 -- TODO:
---  1. add colorscheme plugin to auto update other apps' themes
+--  X. add colorscheme plugin to auto update other apps' themes -- it only took me like 10 hours
 --  2. figure out why is the sign column not working properly for modus theme
 --  3. add proper modus themes for other apps
---  4. update hte lualine. Don't need that breadcrumbs thing
+--  4. update the lualine. Don't need that breadcrumbs thing
 --  5. figure out why is saving twice
 --  6. why does the homepage show the signcolumn?
 --  7. why does neovim resize the terminal?
