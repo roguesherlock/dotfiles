65a73b452438543b56f0cfc5b7557e25de72cb1f
Akash <aakash@hey.com>
2024-12-14 21:59:06 +0530

    remove old stuff

--- .config/nvim-bak/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-vim.g.mapleader = ","
-vim.g.maplocalleader = "\\"
-vim.opt.list = false
--- Show the current document symbols location from Trouble in lualine
--- You can disable this for a buffer by setting `vim.b.trouble_lualine = false`
-vim.g.trouble_lualine = false
