c27ffd97e7ceb95e051ae0978817532c45de4a78
Akash <aakash@hey.com>
2024-12-23 09:03:36 +0530

    update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,25 +3,25 @@ return {
     "miikanissi/modus-themes.nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-      on_highlights = function(h, c)
-        h.LeapLabel = { fg = c.fg_main, bg = c.bg_yellow_intense }
-        local bg = h.LineNr and h.LineNr.bg or c.bg_main
-        h.DiagnosticSignWarn = { bg = bg, fg = c.yellow }
-        h.DiagnosticSignError = { bg = bg, fg = c.red }
-        h.DiagnosticSignHint = { bg = bg, fg = c.cyan }
-        h.DiagnosticSignInfo = { bg = bg, fg = c.blue }
-        -- NOTE: this is for all the todo diagnostics
-        -- h.SignColumn = { bg = bg }
-        -- h.TodoSignTEST = { bg = bg, fg = c.red }
-        -- h.TodoSignPERF = { bg = bg, fg = c.yellow }
-        -- h.TodoSignFIX = { bg = bg, fg = c.green }
-        -- h.TodoSignWARN = { bg = bg, fg = c.yellow }
-        -- h.TodoSignHACK = { bg = bg, fg = c.red }
-        -- h.TodoSignNOTE = { bg = bg, fg = c.cyan }
-        -- h.TodoSignTODO = { bg = bg, fg = c.blue }
-      end,
-    },
+    -- opts = {
+    --   on_highlights = function(h, c)
+    --     h.LeapLabel = { fg = c.fg_main, bg = c.bg_yellow_intense }
+    --     local bg = h.LineNr and h.LineNr.bg or c.bg_main
+    --     h.DiagnosticSignWarn = { bg = bg, fg = c.yellow }
+    --     h.DiagnosticSignError = { bg = bg, fg = c.red }
+    --     h.DiagnosticSignHint = { bg = bg, fg = c.cyan }
+    --     h.DiagnosticSignInfo = { bg = bg, fg = c.blue }
+    --     -- NOTE: this is for all the todo diagnostics
+    --     -- h.SignColumn = { bg = bg }
+    --     -- h.TodoSignTEST = { bg = bg, fg = c.red }
+    --     -- h.TodoSignPERF = { bg = bg, fg = c.yellow }
+    --     -- h.TodoSignFIX = { bg = bg, fg = c.green }
+    --     -- h.TodoSignWARN = { bg = bg, fg = c.yellow }
+    --     -- h.TodoSignHACK = { bg = bg, fg = c.red }
+    --     -- h.TodoSignNOTE = { bg = bg, fg = c.cyan }
+    --     -- h.TodoSignTODO = { bg = bg, fg = c.blue }
+    --   end,
+    -- },
   },
   {
     "LazyVim/LazyVim",
