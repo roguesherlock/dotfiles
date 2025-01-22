fb881b13ba224e072a0646c9e0019ea8135e75f2
Akash <aakash@hey.com>
2025-01-22 13:17:50 +0530

    update

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.snacks_animate = false
+if vim.g.neovide then
+  vim.o.guifont = "Jetbrains Mono:h13"
+  vim.opt.linespace = 2
+  vim.g.neovide_refresh_rate = 120
+end
