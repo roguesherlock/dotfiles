c1d6b562eed016d964a78159975c54d01dfb663f
Akash <aakash@hey.com>
2025-02-15 21:36:07 +0530

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -18,8 +18,9 @@ end
 config.term = "wezterm"
 
 -- config.font = wezterm.font("Geist Mono", { weight = "Medium" })
-config.font_size = 13.0
-config.line_height = 1.2
+config.font = wezterm.font("Iosevka Term")
+config.font_size = 14.0
+-- config.line_height = 1.2
 
 -- config.colors = colors
 -- config.window_frame = window_frame
