d5f57a7c17be642054d353437b9de6e13bea36aa
Akash <aakash@hey.com>
2025-02-28 15:16:24 +0530

    update

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,54 +1,54 @@
 return {
-  {
-    "giuxtaposition/blink-cmp-copilot",
-    enabled = false,
-  },
-  {
-    "saghen/blink.cmp",
-    dependencies = { "fang2hou/blink-copilot" },
-    opts = {
-      sources = {
-        default = { "copilot" },
-        providers = {
-          copilot = {
-            name = "copilot",
-            module = "blink-copilot",
-            score_offset = 100,
-            async = true,
-          },
-        },
-      },
-    },
-  },
-  {
-    "zbirenbaum/copilot.lua",
-    enabled = false,
-  },
-  {
-    "github/copilot.vim",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "BufWinEnter",
-    init = function()
-      if vim.g.ai_cmp then
-        vim.g.copilot_no_maps = true
-      end
-    end,
-    config = function()
-      if vim.g.ai_cmp then
-        -- Block the normal Copilot suggestions
-        vim.api.nvim_create_augroup("github_copilot", { clear = true })
-        for _, event in pairs({ "FileType", "BufUnload", "BufEnter" }) do
-          vim.api.nvim_create_autocmd({ event }, {
-            group = "github_copilot",
-            callback = function()
-              vim.fn["copilot#On" .. event]()
-            end,
-          })
-        end
-      end
-    end,
-  },
+  -- {
+  --   "giuxtaposition/blink-cmp-copilot",
+  --   enabled = false,
+  -- },
+  -- {
+  --   "saghen/blink.cmp",
+  --   dependencies = { "fang2hou/blink-copilot" },
+  --   opts = {
+  --     sources = {
+  --       default = { "copilot" },
+  --       providers = {
+  --         copilot = {
+  --           name = "copilot",
+  --           module = "blink-copilot",
+  --           score_offset = 100,
+  --           async = true,
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   enabled = false,
+  -- },
+  -- {
+  --   "github/copilot.vim",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   event = "BufWinEnter",
+  --   init = function()
+  --     if vim.g.ai_cmp then
+  --       vim.g.copilot_no_maps = true
+  --     end
+  --   end,
+  --   config = function()
+  --     if vim.g.ai_cmp then
+  --       -- Block the normal Copilot suggestions
+  --       vim.api.nvim_create_augroup("github_copilot", { clear = true })
+  --       for _, event in pairs({ "FileType", "BufUnload", "BufEnter" }) do
+  --         vim.api.nvim_create_autocmd({ event }, {
+  --           group = "github_copilot",
+  --           callback = function()
+  --             vim.fn["copilot#On" .. event]()
+  --           end,
+  --         })
+  --       end
+  --     end
+  --   end,
+  -- },
   {
     "dlants/magenta.nvim",
     enabled = false,
