6d53279571e4ad9507a9c10df2dd3483f9ed2a32
Akash <aakash@hey.com>
2025-07-16 22:14:00 +0530

    update

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -85,4 +85,97 @@ return {
       vim.cmd([[cab cc CodeCompanion]])
     end,
   },
+
+  {
+    "dlants/magenta.nvim",
+    enabled = true,
+    lazy = false, -- you could also bind to <leader>mt
+    build = "bun install --frozen-lockfile",
+    keys = {
+      { "<leader>aa", "<cmd>Magenta toggle<cr>", mode = { "n" }, desc = "Toggle [A]i chat" },
+      { "<leader>an", "<cmd>Magenta new-thread<cr>", mode = { "n" }, desc = "[A]i [N]ew thread" },
+      { "<leader>as", "<cmd>Magenta abort<cr>", mode = { "n" }, desc = " [A]i [S]top current operation" },
+      { "<leader>ac", "<cmd>Magenta clear<cr>", mode = { "n" }, desc = "[A]i [C]lear state" },
+      { "<leader>ae", "<cmd>Magenta start-inline-edit<cr>", mode = { "n" }, desc = "[A]i [E]dit inline" },
+      { "<leader>ae", "<cmd>Magenta start-inline-edit-selection<cr>", mode = { "v" }, desc = "[A]i [E]dit inline" },
+      {
+        "<leader>ab",
+        "<cmd>Magenta paste-selection<cr>",
+        mode = { "v" },
+        desc = "Add selection to [A]i [C]hat buffer",
+      },
+      {
+        "<leader>ab",
+        function()
+          require("magenta.actions").add_buffer_to_context()
+        end,
+        mode = { "n" },
+        desc = "[A]i add [B]uffer to chat",
+      },
+      {
+        "<leader>ab",
+        "<cmd>Magenta paste-selection<cr>",
+        mode = { "v" },
+        desc = "Add selection to [A]i [C]hat buffer",
+      },
+      {
+        "<leader>ab",
+        function()
+          require("magenta.actions").add_buffer_to_context()
+        end,
+        mode = { "n" },
+        desc = "[A]i add [B]uffer to chat",
+      },
+      {
+        "<leader>af",
+        function()
+          require("magenta.actions").pick_context_files()
+        end,
+        mode = { "n" },
+        desc = "[A]i add [F]iles to chat",
+      },
+      {
+        "<leader>ap",
+        function()
+          require("magenta.actions").pick_provider()
+        end,
+        mode = { "n" },
+        desc = "[A]i pick [P]rovider",
+      },
+    },
+    opts = {
+      default_keymaps = false,
+      sidebarPosition = "right",
+      sidebarKeymaps = {
+        normal = {
+          ["<CR>"] = ":Magenta send<CR>",
+          ["q"] = ":Magenta toggle<CR>",
+        },
+      },
+      displayKeymaps = {
+        normal = {
+          ["-"] = ":Magenta threads-overview<CR>",
+          ["q"] = ":Magenta toggle<CR>",
+        },
+      },
+      inlineKeymaps = {
+        normal = {
+          ["<CR>"] = function(target_bufnr)
+            vim.cmd("Magenta submit-inline-edit " .. target_bufnr)
+          end,
+          ["q"] = function(target_bufnr)
+            pcall(vim.api.nvim_buf_delete, target_bufnr, { force = true })
+          end,
+        },
+        autoContext = {
+          "context.md",
+          "claude.md",
+          "agent.md",
+          "agents.md",
+          ".cursor/rules",
+          ".magenta/*.md",
+        },
+      },
+    },
+  },
 }
