407a260613ef74bd8a1be455991649d68285e1f5
Akash <aakash@hey.com>
2025-07-24 08:47:38 +0530

    update

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -67,33 +67,7 @@ local vtsls_config = {
   filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 }
 
-local vue_ls_config = {
-  on_init = function(client)
-    client.handlers["tsserver/request"] = function(_, result, context)
-      local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
-      if #clients == 0 then
-        vim.notify("Could not found `vtsls` lsp client, vue_lsp would not work without it.", vim.log.levels.ERROR)
-        return
-      end
-      local ts_client = clients[1]
-
-      local param = unpack(result)
-      local id, command, payload = unpack(param)
-      ts_client:exec_cmd({
-        title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
-        command = "typescript.tsserverRequest",
-        arguments = {
-          command,
-          payload,
-        },
-      }, { bufnr = context.bufnr }, function(_, r)
-        local response_data = { { id, r.body } }
-        ---@diagnostic disable-next-line: param-type-mismatch
-        client:notify("tsserver/response", response_data)
-      end)
-    end
-  end,
-}
+local vue_ls_config = {}
 
 vim.lsp.config("vtsls", vtsls_config)
 vim.lsp.config("vue_ls", vue_ls_config)
