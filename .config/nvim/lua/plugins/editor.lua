6f119af3b44eb45690da802e2977e9cc6ee516c9
Akash <aakash@hey.com>
2025-09-10 18:08:21 +0530

    update

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -69,17 +69,7 @@ return {
     "j-hui/fidget.nvim",
     enabled = true,
     opts = {
-      progress = {
-        display = {
-          progress_icon = {
-            "star",
-          },
-        },
-      },
       notification = {
-        window = {
-          x_padding = 2,
-        },
         override_vim_notify = true, -- Automatically override vim.notify() with Fidget
       },
     },
