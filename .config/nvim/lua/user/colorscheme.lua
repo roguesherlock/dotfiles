5fdda5cfe4899e3cf5313575b3412c25c2e0008e
Akash <aakash@hey.com>
2025-10-11 10:30:51 +0530

    update

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -111,47 +111,6 @@ function M.setup(config)
       end)
     end,
   })
-  -- vim.api.nvim_create_autocmd("ColorScheme", {
-  --   pattern = "*",
-  --   group = group,
-  --   callback = function()
-  --     vim.schedule(function()
-  --       local light = vim.o.background == "light"
-  --       -- Set underline to undercurls
-  --       vim.cmd([[ highlight Underlined cterm=undercurl gui=undercurl ]])
-  --       vim.cmd([[ highlight @markup.underline cterm=undercurl gui=undercurl ]])
-  --       vim.cmd([[ highlight DiagnosticUnderlineOk cterm=undercurl gui=undercurl ]])
-  --       vim.cmd([[ highlight DiagnosticUnderlineHint cterm=undercurl gui=undercurl ]])
-  --       vim.cmd([[ highlight DiagnosticUnderlineInfo cterm=undercurl gui=undercurl ]])
-  --       vim.cmd([[ highlight DiagnosticUnderlineWarn cterm=undercurl gui=undercurl ]])
-  --       vim.cmd([[ highlight DiagnosticUnderlineError cterm=undercurl gui=undercurl ]])
-  --
-  --       -- Defer terminal theme updates
-  --       -- Update Ghostty
-  --       -- TODO: there's a bug where in ghostty where if we set both light,dark themes then it doesn't update the tab color when switching themes
-  --       -- M.set_ghostty_theme(M.config.ghostty)
-  --
-  --       -- Update other terminals
-  --       if light then
-  --         vim.fn.system("kitty +kitten themes --reload-in=all " .. M.config.kitty.light)
-  --         vim.fn.system("kitten @ load-config")
-  --         M.set_ghostty_theme(M.config.ghostty.light)
-  --         M.set_zellij_theme(M.config.zellij.light)
-  --         M.set_delta_theme(M.config.delta.light)
-  --         M.set_yazi_theme(M.config.yazi.light)
-  --         -- M.set_wezterm_theme(M.config.wezterm.light)
-  --       else
-  --         vim.fn.system("kitty +kitten themes --reload-in=all " .. M.config.kitty.dark)
-  --         vim.fn.system("kitten @ load-config")
-  --         M.set_ghostty_theme(M.config.ghostty.dark)
-  --         M.set_zellij_theme(M.config.zellij.dark)
-  --         M.set_delta_theme(M.config.delta.dark)
-  --         M.set_yazi_theme(M.config.yazi.dark)
-  --         -- M.set_wezterm_theme(M.config.wezterm.dark)
-  --       end
-  --     end)
-  --   end,
-  -- })
 
   vim.api.nvim_create_user_command("Light", function()
     M.set_colorscheme(true)
@@ -162,14 +121,11 @@ function M.setup(config)
 end
 
 function M.set_ghostty_theme(theme)
-  -- vim.fn.system("sed -i'.bak' 's/theme = .*/theme = " .. ghostty_light_theme .. "/' (readlink ~/.config/ghostty/config)")
   local config_path = vim.fn.expand("~/.config/ghostty/config")
   local real_path = vim.fn.resolve(config_path)
-  local cmd = string.format("sed -i'.bak' 's/^[ ]*theme[ ]*=.*$/theme = %s/' %s", theme, real_path)
-  -- NOTE: There's a bug in ghostty where if we set both light,dark themes then it doesn't update the tab color when switching themes
-  -- local light_dark_theme = string.format("light:%s,dark:%s", theme.light, theme.dark)
-  -- local cmd = string.format("sed -i'.bak' 's/theme = .*/theme = %s/' %s", light_dark_theme, real_path)
-  -- local cmd = string.format("sed -i'.bak' 's/^[ ]*theme[ ]*=.*$/theme = %s/' %s", light_dark_theme, real_path)
+  -- local cmd = string.format("sed -i'.bak' 's/^[ ]*theme[ ]*=.*$/theme = %s/' %s", theme, real_path)
+  local light_dark_theme = string.format("light:%s,dark:%s", theme.light, theme.dark)
+  local cmd = string.format("sed -i'.bak' 's/^[ ]*theme[ ]*=.*$/theme = %s/' %s", light_dark_theme, real_path)
   local result = vim.fn.system({ "bash", "-c", cmd })
   -- Get Ghostty PID(s) from system command
   local output = vim.fn.system("ps -axo pid=,args= | grep -i ghostty | grep -v grep")
@@ -265,16 +221,13 @@ M.setup_termbg_sync = function(group)
     vim.cmd([[ highlight DiagnosticUnderlineWarn cterm=undercurl gui=undercurl ]])
     vim.cmd([[ highlight DiagnosticUnderlineError cterm=undercurl gui=undercurl ]])
 
-    -- Defer terminal theme updates
     -- Update Ghostty
-    -- TODO: there's a bug where in ghostty where if we set both light,dark themes then it doesn't update the tab color when switching themes
-    -- M.set_ghostty_theme(M.config.ghostty)
+    M.set_ghostty_theme(M.config.ghostty)
 
     -- Update other terminals
     if light then
       vim.fn.system("kitty +kitten themes --reload-in=all " .. M.config.kitty.light)
       vim.fn.system("kitten @ load-config")
-      M.set_ghostty_theme(M.config.ghostty.light)
       M.set_zellij_theme(M.config.zellij.light)
       M.set_delta_theme(M.config.delta.light)
       M.set_yazi_theme(M.config.yazi.light)
@@ -282,7 +235,6 @@ M.setup_termbg_sync = function(group)
     else
       vim.fn.system("kitty +kitten themes --reload-in=all " .. M.config.kitty.dark)
       vim.fn.system("kitten @ load-config")
-      M.set_ghostty_theme(M.config.ghostty.dark)
       M.set_zellij_theme(M.config.zellij.dark)
       M.set_delta_theme(M.config.delta.dark)
       M.set_yazi_theme(M.config.yazi.dark)
